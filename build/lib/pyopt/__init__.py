75a3d4737e29a7f0994512c400a04f9b0e431fd1
jhell1717 <jhellewell1722@gmail.com>
2025-07-29 21:42:21 +0100

    removed build ref

--- build/lib/pyopt/__init__.py
+++
@@ -1,5 +0,0 @@
-from .blackbox import black_box_function
-from .data import Data
-from .gpr import GP
-from .optimiser import Optimiser
-from .utils import plot_gp_optim, plot_gp_plain
--- build/lib/pyopt/blackbox.py
+++
@@ -1,5 +0,0 @@
-import torch
-
-# Define the black-box function (1D example)
-def black_box_function(x):
-    return torch.sin(3 * x) + 0.3 * torch.cos(10 * x)
